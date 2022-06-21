cf100b125b6c43683994881ec72d7fb69c095eba
cccccccccccccccccccccccccccccccccccccc <linus3389@gmail.com>
2022-06-22 01:53:52 +0800

    vercel api hello

--- api/index.go
+++ api/index.go
@@ -6,6 +6,5 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, "hello world")
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
